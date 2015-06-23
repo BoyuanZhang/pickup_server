ce307e646dba0026d2965ea8e76237d13acb90f2
Boyuan Zhang <zhang_boyuan@hotmail.com>
2015-06-23 03:29:07 -0400

    forgot to set up cloudamqp url in MQ settings

--- server/messagequeue/settings.js
+++ server/messagequeue/settings.js
@@ -2,7 +2,7 @@ module.exports = {
 	config: {
 		port: '5672',
 		timeout: '10000',
-		host: process.env.MONGOLAB_URI || 'amqp://127.0.0.1'
+		host: process.env.CLOUDAMQP_URL || 'amqp://127.0.0.1'
 	},
 	queues: {
 		defaultName: 'default-queue'
