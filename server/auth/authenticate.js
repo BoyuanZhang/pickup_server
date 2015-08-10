0a578a475715fe03b5aca356724a0dade8184180
Boyuan Zhang <zhang_boyuan@hotmail.com>
2015-08-10 01:58:40 -0400

    Implemented basic authentication for using server REST API

--- server/auth/authenticate.js
+++ server/auth/authenticate.js
@@ -1,5 +1,33 @@
+var paths = require('../paths'),
+	auth = require(paths.security + '/auth');
+
 function authenticate(req, res, next) {
-	next();
+	var query = req.query, facebookuser;
+
+	if( !validateReq(query) ) {
+		res.statusCode = 400;
+		res.end();
+		return;
+	}
+
+	facebookuser = query.facebookuser ? query.facebookuser : false;
+
+	if( auth.validateToken(query.email, query.authtoken, facebookuser))
+		next();
+	else {
+		res.statusCode = 403;
+		res.end();
+	}
+}
+
+function validateReq(query) {
+	if(!query || !query.email || !query.authtoken)
+		return false;
+
+	if(query.facebookuser && (query.facebookuser !== 'true' || query.facebookuser !== 'false'))
+		return false;
+
+	return true;
 }
 
 module.exports = authenticate;
